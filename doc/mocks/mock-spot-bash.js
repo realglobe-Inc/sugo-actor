afacaf66956303ce2b419b26c4a20f9855a962ff
Taka Okunishi <okunishitaka.com@gmail.com>
2016-05-30 10:01:25 +0900

    Modify travis settings

--- doc/mocks/mock-spot-bash.js
+++ doc/mocks/mock-spot-bash.js
@@ -1,21 +1,37 @@
 'use strict'
 
+const co = require('co')
+const childProcess = require('child_process')
+
 module.exports = function mockSpotBash () {
   /**
    * Handle bash commands
-   * @param {string} cmd - Name of command
+   * @param {Object} ctx - Invoke context
    * @returns {Promise}
    */
-  function bash (cmd) {
-
+  function bash (ctx) {
+    let { cmd, params, socket } = ctx
+    switch (cmd) {
+      case 'exec':
+        return co(function * () {
+          return yield new Promise((resolve, reject) => {
+            let spawned = childProcess.spawn(cmd)
+            spawned.stdout.on('data', (data) => socket.emit('stdout', data))
+            spawned.stderr.on('data', (data) => socket.emit('stderr', data))
+            socket.on('stdin', (data) => spawned.stdin.write(data))
+            spawned.on('error', (err) => reject(err))
+            spawned.on('close', (code) => resolve(code))
+          })
+        })
+      default :
+        throw new Error(`Unknown command: ${cmd}`)
+    }
   }
 
   return Object.assign(bash, {
     $desc: 'Bash interface',
     $methods: {
-      exec: {
-        $params: []
-      }
+      exec: { $desc: 'Execute a command' }
     }
   })
 }
