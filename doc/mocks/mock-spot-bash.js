3e8768274585e21554bd0b839c542e711561019d
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-02 17:23:17 +0900

    Add action handler

--- doc/mocks/mock-spot-bash.js
+++ doc/mocks/mock-spot-bash.js
@@ -4,18 +4,14 @@ const co = require('co')
 const childProcess = require('child_process')
 
 module.exports = function mockSpotBash () {
-  /**
-   * Handle bash commands
-   * @param {Object} ctx - Invoke context
-   * @returns {Promise}
-   */
-  function bash (ctx) {
-    let { cmd, params, pipe } = ctx
-    switch (cmd) {
-      case 'exec':
+  return {
+    $actions: {
+      spawn (ctx) {
+        let { params, pipe } = ctx
         return co(function * () {
           return yield new Promise((resolve, reject) => {
-            let spawned = childProcess.spawn(cmd)
+            let { cmd, args, options } = params
+            let spawned = childProcess.spawn(cmd, args, options)
             spawned.stdout.on('data', (data) => pipe.emit('stdout', data))
             spawned.stderr.on('data', (data) => pipe.emit('stderr', data))
             pipe.on('stdin', (data) => spawned.stdin.write(data))
@@ -23,22 +19,20 @@ module.exports = function mockSpotBash () {
             spawned.on('close', (code) => resolve(code))
           })
         })
-      default :
-        throw new Error(`Unknown command: ${cmd}`)
-    }
-  }
-
-  return Object.assign(bash, {
-    spec: {
-      desc: 'Bash interface',
-      methods: {
+      }
+    },
+    $spec: {
+      $desc: 'Bash interface',
+      $methods: {
         exec: {
-          desc: 'Execute a command',
-          params: [
-            { name: 'script', type: 'string' }
+          $desc: 'Spawn a command',
+          $params: [
+            { $name: 'cmd', $type: 'string', $desc: 'Command to spawn' },
+            { $name: 'args', $type: 'array', $desc: 'Command arguments' },
+            { $name: 'options', $type: 'Object', $desc: 'Optional settings' }
           ]
         }
       }
     }
-  })
+  }
 }
--- test/sugo_spot_test.js
+++ test/sugo_spot_test.js
@@ -6,7 +6,9 @@
 
 const SugoSpot = require('../lib/sugo_spot.js')
 const sgSocket = require('sg-socket')
+const sgSocketClient = require('sg-socket-client')
 const assert = require('assert')
+const apemansleep = require('apemansleep')
 const co = require('co')
 
 const { GreetingEvents, RemoteEvents, AcknowledgeStatus } = require('sg-socket-constants')
@@ -16,23 +18,41 @@ const { OK, NG } = AcknowledgeStatus
 const { INTERFACE, ACTION } = RemoteEvents
 
 describe('sugo-spot', () => {
+  let sleep = apemansleep.create({})
   let port = 9872
   let server
   before(() => co(function * () {
     server = sgSocket(port)
-    server.on('connection', (socket) => {
-      socket.on(HI, (data, callback) => callback(OK))
-      socket.on(BYE, (data, callback) => callback(OK))
-      socket.on(INTERFACE, (data, callback) => callback(OK))
+    server.of('spots').on('connection', (socket) => {
+      socket.on(HI, (data, callback) => callback({ status: OK }))
+      socket.on(BYE, (data, callback) => callback({ status: OK }))
+      socket.on(INTERFACE, (data, callback) => callback({ status: OK }))
+      setTimeout(() => co(function * () {
+        console.log('!!hoge')
+        socket.eimt(ACTION, {
+          target: 'bash',
+          name: 'spawn',
+          params: {
+            cmd: 'ls',
+            args: '-la',
+            options: {}
+          }
+        })
+      }), 100)
     })
   }))
 
   after(() => co(function * () {
-    yield new Promise((resolve) => server.close(resolve()))
+    yield new Promise((resolve) => {
+      setTimeout(() => {
+        server.close(resolve())
+      }, 200)
+    })
   }))
 
   it('Sugo spot', () => co(function * () {
-    let spot = new SugoSpot(`http://localhost:${port}`, {
+    let url = `http://localhost:${port}/spots`
+    let spot = new SugoSpot(url, {
       key: 'hogehoge',
       interfaces: {
         bash: require('../doc/mocks/mock-spot-bash')()
@@ -40,6 +60,8 @@ describe('sugo-spot', () => {
     })
 
     yield spot.connect()
+
+    yield sleep.sleep(100)
     yield spot.disconnect()
   }))
 })
