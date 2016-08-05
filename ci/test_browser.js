179f3805fa527a037516deecad09b84a6e984608
FujiHaruka <fujiharuka@realglobe.jp>
2016-08-05 14:53:09 +0900

    Add browser tests with karma

---
+++ ci/test_browser.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+/**
+ * Run browser tests.
+ */
+
+'use strict'
+
+process.chdir(`${__dirname}/..`)
+process.env.DEBUG = 'sg:*'
+
+const apeTasking = require('ape-tasking')
+const co = require('co')
+const sgSocket = require('sg-socket')
+const {exec} = require('child_process')
+
+const { GreetingEvents, RemoteEvents, AcknowledgeStatus } = require('sg-socket-constants')
+
+const { HI, BYE } = GreetingEvents
+const { OK, NG } = AcknowledgeStatus
+const { SPEC, PERFORM, PIPE } = RemoteEvents
+
+let server
+let port = 9872
+
+apeTasking.runTasks('browser test', [
+  () => new Promise((resolve, reject) => {
+    exec('./ci/browser.js', (err, stdout) => {
+      err ? reject(err) : resolve(stdout)
+    })
+  }),
+  () => co(function * () {
+    server = sgSocket(port)
+    server.of('/actors').on('connection', (socket) => {
+      socket.on(HI, (data, callback) => {
+        callback({ status: OK, payload: { key: data.key } })
+      })
+      socket.on(BYE, (data, callback) => {
+        callback({ status: OK })
+      })
+      socket.on(SPEC, (data, callback) => {
+        callback({ status: OK })
+      })
+      socket.on(PIPE, (data) => {
+      })
+    })
+  }),
+  () => new Promise((resolve, reject) => {
+    exec('./node_modules/.bin/karma start', (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      }
+      console.log(stdout)
+      console.error(stderr)
+      resolve()
+    })
+  }),
+  () => co(function * () {
+    server.close()
+  })
+], true)
---
+++ test/ws_emitter_test.karma.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for wsEmitter.
+ * Runs with karma.
+ */
+'use strict'
+
+const wsEmitter = require('../shim/browser/emitting/ws_emitter.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('ws-emitter', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Ws emitter', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
